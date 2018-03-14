fe3f2a686c37d1a9ef34d3d86cf0390af15ab6d3
McJty <mcjty1@gmail.com>
2018-03-14 19:29:01 +0100

    0.0.2

--- src/main/java/mcjty/rftoolspower/RFToolsPower.java
+++ src/main/java/mcjty/rftoolspower/RFToolsPower.java
@@ -21,7 +21,7 @@ import net.minecraftforge.fml.common.event.*;
         version = RFToolsPower.VERSION)
 public class RFToolsPower implements ModBase {
     public static final String MODID = "rftoolspower";
-    public static final String VERSION = "0.0.1";
+    public static final String VERSION = "0.0.2";
     public static final String MIN_FORGE_VER = "14.22.0.2464";
     public static final String MIN_MCJTYLIB_VER = "2.6.3";
 
