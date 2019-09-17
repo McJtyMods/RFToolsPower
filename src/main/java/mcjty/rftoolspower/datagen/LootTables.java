cb3ccab43e0aa783a0c50798f17381dd4d2c2008
McJty <mcjty1@gmail.com>
2019-09-17 05:09:41 +0200

    More work on recipes, loot tables and advancements

--- src/main/java/mcjty/rftoolspower/datagen/LootTables.java
+++ src/main/java/mcjty/rftoolspower/datagen/LootTables.java
@@ -1,16 +1,26 @@
 package mcjty.rftoolspower.datagen;
 
+import mcjty.lib.datagen.BaseLootTableProvider;
+import mcjty.rftoolspower.blocks.ModBlocks;
 import net.minecraft.data.DataGenerator;
-import net.minecraft.data.DirectoryCache;
-import net.minecraft.data.LootTableProvider;
 
-public class LootTables extends LootTableProvider {
+public class LootTables extends BaseLootTableProvider {
 
     public LootTables(DataGenerator dataGeneratorIn) {
         super(dataGeneratorIn);
     }
 
     @Override
-    public void act(DirectoryCache cache) {
+    protected void addTables() {
+        lootTables.put(ModBlocks.COALGENERATOR, createStandardTable("coalgenerator", ModBlocks.COALGENERATOR));
+        lootTables.put(ModBlocks.CELL1, createStandardTable("cell1", ModBlocks.CELL1));
+        lootTables.put(ModBlocks.CELL2, createStandardTable("cell2", ModBlocks.CELL2));
+        lootTables.put(ModBlocks.CELL3, createStandardTable("cell3", ModBlocks.CELL3));
+        lootTables.put(ModBlocks.INFORMATION_SCREEN, createSimpleTable("informationscreen", ModBlocks.INFORMATION_SCREEN));
+    }
+
+    @Override
+    public String getName() {
+        return "RFToolsPower LootTables";
     }
 }
