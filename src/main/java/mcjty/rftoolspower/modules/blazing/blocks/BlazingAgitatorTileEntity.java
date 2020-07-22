678f47c08b383ee8b4d89897acb3796797219b90
mcjty <slto15aGH10>
2020-07-22 16:55:16 +0200

    More work on the blazing agitator

--- src/main/java/mcjty/rftoolspower/modules/blazing/blocks/BlazingAgitatorTileEntity.java
+++ src/main/java/mcjty/rftoolspower/modules/blazing/blocks/BlazingAgitatorTileEntity.java
@@ -21,6 +21,7 @@ import mcjty.lib.typed.Type;
 import mcjty.rftoolspower.compat.RFToolsPowerTOPDriver;
 import mcjty.rftoolspower.modules.blazing.BlazingConfiguration;
 import mcjty.rftoolspower.modules.blazing.BlazingSetup;
+import mcjty.rftoolspower.modules.blazing.items.BlazingRod;
 import net.minecraft.block.Block;
 import net.minecraft.block.BlockState;
 import net.minecraft.block.SoundType;
@@ -29,7 +30,6 @@ import net.minecraft.inventory.container.INamedContainerProvider;
 import net.minecraft.item.ItemStack;
 import net.minecraft.item.Items;
 import net.minecraft.nbt.CompoundNBT;
-import net.minecraft.nbt.INBT;
 import net.minecraft.tileentity.ITickableTileEntity;
 import net.minecraft.util.Direction;
 import net.minecraft.util.math.BlockPos;
@@ -148,8 +148,93 @@ public class BlazingAgitatorTileEntity extends GenericTileEntity implements ITic
     public void tick() {
         if (!world.isRemote) {
             if (isMachineEnabled()) {
+                if (storage.getEnergy() >= BlazingConfiguration.AGITATOR_USE_PER_TICK.get()) {
+                    storage.consumeEnergy(BlazingConfiguration.AGITATOR_USE_PER_TICK.get());
+                    for (int i = 0 ; i < BUFFER_SIZE ; i++) {
+                        ItemStack stack = items.getStackInSlot(i);
+                        if (!stack.isEmpty()) {
+                            if (stack.getItem() == Items.BLAZE_ROD) {
+                                items.setStackInSlot(i, new ItemStack(BlazingSetup.BLAZING_ROD.get()));
+                                markDirtyQuick();
+                            } else {
+                                float timeLeft = BlazingRod.getAgitationTimeLeft(stack);
+                                if (timeLeft > 0) {
+                                    tickBlazingRod(i, stack, timeLeft);
+                                } else if (!locked[i]) {
+                                    moveToOutput(i, stack);
+                                }
+                            }
+                        }
+                    }
+                }
             }
         }
+    }
+
+    private void tickBlazingRod(int i, ItemStack stack, float timeLeft) {
+        float adjacencyFactor = calculateAdjacencyFactor(i);
+        timeLeft -= adjacencyFactor;
+        BlazingRod.setAgitationTimeLeft(stack, timeLeft);
+
+        float powerQuality = BlazingRod.getPowerQuality(stack);
+        powerQuality += timeLeft * adjacencyFactor;
+        BlazingRod.setPowerQuality(stack, powerQuality);
+
+        float powerDuration = BlazingRod.getPowerDuration(stack);
+        powerDuration += timeLeft * adjacencyFactor;
+        BlazingRod.setPowerDuration(stack, powerDuration);
+        markDirtyQuick();
+    }
+
+    private void moveToOutput(int i, ItemStack stack) {
+        // Move the rod to the output area if there is room
+        for (int j = BUFFER_SIZE ; j < BUFFER_SIZE + BUFFER_SIZE ; j++) {
+            if (items.getStackInSlot(j).isEmpty()) {
+                items.setStackInSlot(i, ItemStack.EMPTY);
+                items.setStackInSlot(j, stack);
+                markDirtyQuick();
+                return;
+            }
+        }
+    }
+
+    /// Calculate the quality factor for this rod alone without looking at neighbours
+    private float calculateQualityFactor(int i) {
+        ItemStack stack = items.getStackInSlot(i);
+        if (!stack.isEmpty()) {
+            float duration = BlazingRod.getPowerDuration(stack);
+            float quality = BlazingRod.getPowerQuality(stack) / 1000.0f;
+            return (duration + quality) / 2.0f;
+        }
+        return 0;
+    }
+
+    /// Calculate the difference between the main quality factor and the quality factor for this rod alone and account for empty stack penalty
+    private float calculateQualityFactorDiff(float fThis, int i, int x, int y) {
+        if (x < 0 || y < 0 || x > 2 || y > 2) {
+            return -.1f;      // Penalty for empty slot
+        }
+        ItemStack stack = items.getStackInSlot(i);
+        if (stack.isEmpty()) {
+            return -.1f;      // Penalty for empty slot
+        }
+        return fThis - calculateQualityFactor(i);
+    }
+
+    /// Calculate the quality factor for this slot given the adjacent slots
+    private float calculateAdjacencyFactor(int i) {
+        float fThis = calculateQualityFactor(i);
+        int x = i % 3;
+        int y = i / 3;
+        float factor = 1.0f;
+        factor += calculateQualityFactorDiff(fThis, i - 1, x-1, y);    // Index left
+        factor += calculateQualityFactorDiff(fThis, i + 1, x+1, y);    // Index right
+        factor += calculateQualityFactorDiff(fThis, i - 3, x, y-1);    // Index top
+        factor += calculateQualityFactorDiff(fThis, i + 3, x, y+1);    // Index bottom
+        if (factor < 0.01f) {
+            factor = 0.01f;
+        }
+        return factor / 4;
     }
 
     public boolean isLocked(int x, int y) {
--- src/main/java/mcjty/rftoolspower/modules/blazing/items/BlazingRod.java
+++ src/main/java/mcjty/rftoolspower/modules/blazing/items/BlazingRod.java
@@ -1,6 +1,7 @@
 package mcjty.rftoolspower.modules.blazing.items;
 
 import mcjty.lib.builder.TooltipBuilder;
+import mcjty.lib.varia.NBTTools;
 import mcjty.rftoolspower.setup.Registration;
 import net.minecraft.client.util.ITooltipFlag;
 import net.minecraft.item.Item;
@@ -10,14 +11,17 @@ import net.minecraft.world.World;
 
 import java.util.List;
 
-import static mcjty.lib.builder.TooltipBuilder.header;
-import static mcjty.lib.builder.TooltipBuilder.key;
+import static mcjty.lib.builder.TooltipBuilder.*;
 
 public class BlazingRod extends Item {
 
     private final TooltipBuilder tooltipBuilder = new TooltipBuilder()
             .info(key("message.rftoolsbase.shiftmessage"))
-            .infoShift(header());
+            .infoShift(header(),
+                    parameter("time", stack -> Float.toString(getAgitationTimeLeft(stack))),
+                    parameter("quality", stack -> Float.toString(getPowerQuality(stack))),
+                    parameter("duration", stack -> Float.toString(getPowerDuration(stack))));
+
 
     public BlazingRod() {
         super(Registration.createStandardProperties());
@@ -27,5 +31,32 @@ public class BlazingRod extends Item {
     public void addInformation(ItemStack itemStack, World world, List<ITextComponent> list, ITooltipFlag flags) {
         super.addInformation(itemStack, world, list, flags);
         tooltipBuilder.makeTooltip(getRegistryName(), itemStack, list, flags);
+    }
+
+    // Get time left (in ticks) before ready
+    public static float getAgitationTimeLeft(ItemStack stack) {
+        return NBTTools.getFloat(stack, "time", 20.0f * 120.0f);
+    }
+
+    public static void setAgitationTimeLeft(ItemStack stack, float time) {
+        stack.getOrCreateTag().putFloat("time", time);
+    }
+
+    // Quality is expressed in RF per 1000 ticks
+    public static float getPowerQuality(ItemStack stack) {
+        return NBTTools.getFloat(stack, "quality", 5000f);
+    }
+
+    public static void setPowerQuality(ItemStack stack, float quality) {
+        stack.getOrCreateTag().putFloat("quality", quality);
+    }
+
+    // Duration is expressed in ticks
+    public static float getPowerDuration(ItemStack stack) {
+        return NBTTools.getFloat(stack, "duration", 10f);
+    }
+
+    public static void setPowerDuration(ItemStack stack, float duration) {
+        stack.getOrCreateTag().putFloat("duration", duration);
     }
 }
